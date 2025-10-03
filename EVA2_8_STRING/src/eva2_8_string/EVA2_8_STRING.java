dab836209c84b1d071482c3ddd3b6ff3096de612
AllisonPGarfio <allisonpalomag@gmail.com>
2025-10-03 17:55:35 -0600

    Add files via upload

---
+++ EVA2_8_STRING/src/eva2_8_string/EVA2_8_STRING.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_string;
+
+/**
+ *
+ * @author garfi
+ */
+public class EVA2_8_STRING {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String mensaje = "HOLA MUNDOO";
+        int Valor = 100;
+        System.out.println(mensaje.charAt(7));
+        
+        char caracter = mensaje.charAt(6);
+        System.out.println(caracter);
+    }
+    
+}
